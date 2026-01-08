427ea711194d8d222058129c8f74012c3de41929
take_ryosuke <take.ryosuke@alhinc.jp>
2026-01-08 09:49:14 +0900

    add feature 2-1, 2-2

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +15,9 @@ public class CalculateSales {
 
 	// 支店別集計ファイル名
 	private static final String FILE_NAME_BRANCH_OUT = "branch.out";
+
+	// 売上ファイル名 正規表現
+	private static final String FILE_NAME_REGEX_SALES = "^[0-9]{8}\\.rcd$";
 
 	// エラーメッセージ
 	private static final String UNKNOWN_ERROR = "予期せぬエラーが発生しました";
@@ -30,13 +34,25 @@ public class CalculateSales {
 		Map<String, String> branchNames = new HashMap<>();
 		// 支店コードと売上金額を保持するMap
 		Map<String, Long> branchSales = new HashMap<>();
+		// 売上ファイルを保持するList
+		ArrayList<File> salesFiles = new ArrayList<File>();
 
 		// 支店定義ファイル読み込み処理
-		if (!readFile(args[0], FILE_NAME_BRANCH_LST, branchNames, branchSales)) {
+		if (!readBranchDifinitionFile(args[0], FILE_NAME_BRANCH_LST, branchNames)) {
 			return;
 		}
 
-		// ※ここから集計処理を作成してください。(処理内容2-1、2-2)
+		// (処理内容2-1) 売上ファイルリスト取得処理
+		if (!getSalesFiles(args[0], salesFiles)) {
+			return;
+		}
+
+		// (処理内容2-2) 売上ファイルリスト読み込み処理
+		for (File salesFile : salesFiles) {
+			if (!readSalesFile(salesFile, branchNames, branchSales)) {
+				return;
+			}
+		}
 
 		// 支店別集計ファイル書き込み処理
 		if (!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)) {
@@ -51,11 +67,9 @@ public class CalculateSales {
 	 * @param フォルダパス
 	 * @param ファイル名
 	 * @param 支店コードと支店名を保持するMap
-	 * @param 支店コードと売上金額を保持するMap
 	 * @return 読み込み可否
 	 */
-	private static boolean readFile(String path, String fileName, Map<String, String> branchNames,
-			Map<String, Long> branchSales) {
+	private static boolean readBranchDifinitionFile(String path, String fileName, Map<String, String> branchNames) {
 		BufferedReader br = null;
 
 		try {
@@ -66,15 +80,98 @@ public class CalculateSales {
 			String line;
 			// 一行ずつ読み込む
 			while ((line = br.readLine()) != null) {
-				final Long INITIAL_BRANCH_SALES = (long) 0;
 				// (処理内容1-2)
 				String[] items = line.split(",");
 				String branchCode = items[0];
 				String branchName = items[1];
 
 				branchNames.put(branchCode, branchName);
-				branchSales.put(branchCode, INITIAL_BRANCH_SALES);
 			}
+
+		} catch (IOException e) {
+			System.out.println(UNKNOWN_ERROR);
+			return false;
+		} finally {
+			// ファイルを開いている場合
+			if (br != null) {
+				try {
+					// ファイルを閉じる
+					br.close();
+				} catch (IOException e) {
+					System.out.println(UNKNOWN_ERROR);
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * 売上ファイルリスト取得処理
+	 *
+	 * @param フォルダパス
+	 * @param ファイル名
+	 * @param 売り上げファイルを保持するList
+	 * @return 読み込み可否
+	 */
+	private static boolean getSalesFiles(String path, ArrayList<File> salesFiles) {
+
+		try {
+			File[] files = new File(path).listFiles();
+			for (File file : files) {
+				String fileName = file.getName();
+				if (fileName.matches(FILE_NAME_REGEX_SALES)) {
+					salesFiles.add(file);
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(UNKNOWN_ERROR);
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * 売上ファイル読み込み処理
+	 *
+	 * @param 売り上げファイルを保持するList
+	 * @param 支店コードと支店名を保持するMap
+	 * @param 支店コードと売上金額を保持するMap
+	 * @return 読み込み可否
+	 */
+	private static boolean readSalesFile(File salesFile, Map<String, String> branchNames,
+			Map<String, Long> branchSales) {
+		BufferedReader br = null;
+
+		try {
+			FileReader fr = new FileReader(salesFile);
+			br = new BufferedReader(fr);
+			ArrayList<String> fileContents = new ArrayList<String>();
+			String line;
+
+			// 一行ずつ読み込む
+			while ((line = br.readLine()) != null) {
+				fileContents.add(line);
+			}
+
+			String branchCode = fileContents.get(0);
+			Long fileSale = Long.parseLong(fileContents.get(1));
+
+			// 支店定義ファイルにない支店コードなら合計処理をしない
+			if (!branchNames.containsKey(branchCode)) {
+				return false;
+			}
+
+			// 暫定の合計金額を算出
+			Long saleAmount = (long) 0;
+			if (branchSales.containsKey(branchCode)) {
+				saleAmount = branchSales.get(branchCode);
+			}
+			saleAmount += fileSale;
+
+			// 売り上げの合計金額を更新
+			branchSales.put(branchCode, saleAmount);
 
 		} catch (IOException e) {
 			System.out.println(UNKNOWN_ERROR);
